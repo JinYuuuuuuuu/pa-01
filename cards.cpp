66bbe075cdff4f6956fb89c033f1b9f15f7c7fb3
Hechenjin Yu <hechenjin@cs.ucsb.edu>
2023-11-13 16:37:51 -0800

    1st

--- cards.cpp
+++ cards.cpp
@@ -1,4 +1,100 @@
-// cards.cpp
-// Author: Your name
-// Implementation of the classes defined in cards.h
+#include "cards.h"
 
+// Implement the overloaded operators for Card
+bool Card::operator<(const Card& other) const {
+    if (suit == other.suit) {
+        return value < other.value; // You need to handle card values properly
+    }
+    return suit < other.suit;
+}
+
+bool Card::operator>(const Card& other) const {
+    return other < *this;
+}
+
+bool Card::operator==(const Card& other) const {
+    return suit == other.suit && value == other.value;
+}
+
+// CardBST Destructor
+CardBST::~CardBST() {
+    destroyTree(root);
+}
+
+// Insert a card
+void CardBST::insert(const Card& card) {
+    insertHelper(root, card);
+}
+
+void CardBST::insertHelper(Node*& node, const Card& card) {
+    if (node == nullptr) {
+        node = new Node(card);
+    } else if (card < node->card) {
+        insertHelper(node->left, card);
+    } else if (card > node->card) {
+        insertHelper(node->right, card);
+    }
+}
+
+// Find a card
+bool CardBST::find(const Card& card) const {
+    Node* current = root;
+    while (current != nullptr) {
+        if (card == current->card) {
+            return true;
+        } else if (card < current->card) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+    return false;
+}
+
+// Remove a card
+void CardBST::remove(const Card& card) {
+    root = removeHelper(root, card);
+}
+
+Node* CardBST::removeHelper(Node* node, const Card& card) {
+    if (node == nullptr) return node;
+
+    if (card < node->card) {
+        node->left = removeHelper(node->left, card);
+    } else if (card > node->card) {
+        node->right = removeHelper(node->right, card);
+    } else {
+        if (node->left == nullptr) {
+            Node* temp = node->right;
+            delete node;
+            return temp;
+        } else if (node->right == nullptr) {
+            Node* temp = node->left;
+            delete node;
+            return temp;
+        }
+
+        Node* temp = findMin(node->right);
+        node->card = temp->card;
+        node->right = removeHelper(node->right, temp->card);
+    }
+    return node;
+}
+
+Node* CardBST::findMin(Node* node) const {
+    Node* current = node;
+    while (current && current->left != nullptr) {
+        current = current->left;
+    }
+    return current;
+}
+
+// Print in-order
+void CardBST::printInOrder() const {
+    printInOrderHelper(root);
+}
+
+void CardBST::printInOrderHelper(Node* node) const {
+    if (node != nullptr) {
+        printInOrderHelper(node->left);
+        std::cout << node->card.suit << " " << node->card.value << std::endl;
--- cards.h
+++ cards.h
@@ -5,4 +5,52 @@
 #ifndef CARDS_H
 #define CARDS_H
 
+#include <iostream>
+#include <string>
+
+// Card struct
+struct Card {
+    std::string suit;
+    std::string value;
+
+    Card(std::string s, std::string v) : suit(s), value(v) {}
+
+    bool operator<(const Card& other) const;
+    bool operator>(const Card& other) const;
+    bool operator==(const Card& other) const;
+};
+
+// Node struct for BST
+struct Node {
+    Card card;
+    Node* left;
+    Node* right;
+
+    Node(Card c) : card(c), left(nullptr), right(nullptr) {}
+};
+
+// Binary Search Tree for Cards
+class CardBST {
+public:
+    CardBST() : root(nullptr) {}
+    ~CardBST();
+
+    void insert(const Card& card);
+    bool find(const Card& card) const;
+    void remove(const Card& card);
+    void printInOrder() const;
+
+private:
+    Node* root;
+
+    void insertHelper(Node*& node, const Card& card);
+    Node* removeHelper(Node* node, const Card& card);
+    Node* findMin(Node* node) const;
+    void printInOrderHelper(Node* node) const;
+    void destroyTree(Node* node);
+};
+
+#endif // CARDS_H
+
+
 #endif
